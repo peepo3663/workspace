cbe5f720010a61ea36d4a360a6522bf100a98d68
masaruz <stampz74@gmail.com>
2013-06-10 10:00:27 +0200

    created new package

---
+++ RestletDriver/src/resources/Record.java
@@ -0,0 +1,26 @@
+package resources;
+
+public class Record {
+	private long timestamp;
+	private String flag;
+	private double value;
+	
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getFlag() {
+		return flag;
+	}
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+}
